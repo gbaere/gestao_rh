0f58dc8653c14dc804a3dba1f9405d0d5fb4bb69
Guilherme Bender <guilherme.baere@gmail.com>
2023-09-26 23:24:04 -0300

    App Colaboradores

---
+++ apps/colaboradores/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Colaborador
+
+admin.site.register(Colaborador)
+
+
+
---
+++ apps/colaboradores/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Colaborador(models.Model):
+    nome = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.nome
+
+
